d45ed21be2729cd81de2074326b44827e2f11864
fredkast <fredericcastelpro@gmail.com>
2024-03-20 14:15:05 +0100

    Form development

--- CyberTeam/src/app/app.component.ts
+++ CyberTeam/src/app/app.component.ts
@@ -1,14 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+
 })
 export class AppComponent {
   title = 'CyberTeam';
